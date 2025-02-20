0d3beaa01a7995dda662afc20d81379215cf3a1f
zer0ken <a37592777@gmail.com>
2025-02-20 10:16:44 +0900

    들여쓰기 잘못된 거 수정

--- resnet18.py
+++ resnet18.py
@@ -24,9 +24,9 @@ def train_model():
     # Step 2: Data Transforms
     # ------------------------
     train_transforms = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-])
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
 
 
     val_transforms = transforms.Compose([
